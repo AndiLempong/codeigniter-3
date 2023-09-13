451e6c9e410ec35a238e900059fd83cc308df91d
AndiLempong <kpes2901@gmail.com>
2023-09-13 14:36:50 +0700

    first commit

---
+++ application/views/home.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PENDAFTARAN ONLINE</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <style>
+        .background {
+            background-image: url('https://foto.data.kemdikbud.go.id/getImage/20328986/7.jpg');
+            background-size: cover;
+        }
+        .background-image-black {
+        height: 100vh;
+        background-color: rgb(0, 0, 0, 0.4);
+    }
+    </style>
+
+</head>
+<body class="background">
+    <div class="background-image-black" >
+
+        <h1 style="text-align: center;">Pendaftaran Online</h1>
+        <hr>
+        <center><img src="https://o.remove.bg/downloads/72f0720b-45d1-4e78-a228-2f1c03987482/logobinusa-removebg-preview.png" alt="" width="350px" height="300px">
+        <h2 style="text-align: center;">SMK Bina Nusantara Semarang</h2>
+        
+        <a class="btn btn-primary" href="auth">Login</a>
+    </center>
+</div>
+    
+</body>
+</html
