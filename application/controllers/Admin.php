b04dff306204d421d4b4cb5eb2801e7ea6754d31
AndiLempong <kpes2901@gmail.com>
2023-09-15 14:52:59 +0700

    update

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Admin extends CI_Controller {
 
 	function __construct(){
+
 		parent::__construct();
 		$this->load->model('m_model');
 		$this->load->helper('my_helper');;
@@ -18,7 +19,34 @@ class Admin extends CI_Controller {
 	}
 	public function daftarsiswa()
 	{
-		$this->load->view('admin/daftarsiswa');
+		$data['siswa'] = $this->m_model->get_data('siswa')->result();
+		$this->load->view('admin/siswa', $data);
 	}
+
+	public function hapus_siswa($id)
+	{
+		$this->m_model->delete('siswa', 'id_siswa', $id);
+		redirect(base_url('admin/siswa'));
+	}
+
+	public function tambah_siswa()
+	{
+		$data['kelas'] = $this->m_model->get_data('kelas')->result();
+		$this->load->view('admin/tambah_siswa', $data);
+	}
+
+	public function aksi_tambah_siswa()
+	{
+		$data = [
+			'nama_siswa' => $this->input->post('nama'),
+			'nisn' => $this->input->post('nisn'),
+			'gender' => $this->input->post('gender'),
+			'id_kelas' =>$this->input->post('kelas')
+		];
+
+		$this->m_model->tambah_data('siswa', $data);
+		redirect(base_url('admin/siswa'));
+	}
+
 }
 ?>
