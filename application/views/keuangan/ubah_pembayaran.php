5f2c85a0a1cbb43ff87db123d1c8fc1b38587201
AndiLempong <kpes2901@gmail.com>
2023-09-30 12:20:46 +0700

    update

--- application/views/keuangan/ubah_pembayaran.php
+++ application/views/keuangan/ubah_pembayaran.php
@@ -1,50 +1,79 @@
 <!DOCTYPE html>
-<html lang='en'>
-
+<html lang="en">
 <head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Daftar</title>
-    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9' crossorigin='anonymous'>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 </head>
 
-<body class="min-vh-100 d-flex align-items-center">
-    <div class="card w-50 m-auto p-3">
-        <h3 class="text-center">Change Payment</h3>
-        <br>
-        <form action="<?php echo base_url('keuangan/aksi_ubah_pembayaran') ?>" method="post" class="row">
-            <div class="mb-3 col-6">
-                <label for="siswa" class="form-label">Siswa</label>
-                <select name="id_siswa" class="form-select">
-                    <option selected>Pilih Siswa</option>
-                    <?php foreach ($siswa as $row) : ?>
-                    <option value="<?php echo $row->id_siswa ?>">
-                        <?php echo $row->nama_siswa ?>
-                    </option>
-                    <?php endforeach ?>
-                </select>
-                <br>
+<body class="p-5 align-items-center">
+    <div class="container-fluid">
+        <div class="row">
+                    <a href="<?php echo base_url('keuangan/pembayaran') ?>">
+                    </a>
+                </div>
             </div>
-            <div class="mb-3 col-6">
-                <label for="jenis_pembayaran" class="form-label"><b>Jenis Pembayaran</b></label>
+                    </div>
+                </div>
 
-                <?php foreach ()
-                <select name="jenis_pembayaran" class="form-select">
-                    <option selected >Pilih Jenis Pembayaran</option>
-                    <option value="Pembayaran SPP">Pembayaran SPP</option>
-                    <option value="Pembayaran Uang Gedung">Pembayaran Uang Gedung</option>
-                    <option value="Pembayaran Seragam">Pembayaran Seragam</option>
-                </select>
-                <br>
+                <div class="card w-50 m-auto p-5 shadow">
+                    <div class="card-body">
+                        <?php foreach ($pembayaran as $data_pembayaran): ?>
+                        <form action="<?php echo base_url('keuangan/aksi_ubah_pembayaran') ?>"
+                            enctype="multipart/form-data" method="POST">
+                            <input name="id" type="hidden" value="<?php echo $data_pembayaran->id ?>">
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="siswa">Siswa</label>
+                                        <select id="siswa" name="siswa" class="form-control">
+                                            <option selected value="<?php $data_pembayaran->id_siswa ?>">
+                                                <?php echo tampil_full_siswa_byid($data_pembayaran->id_siswa) ?>
+                                            </option>
+                                            <?php foreach ($siswa as $row): ?>
+                                            <option value="<?php echo $row->id_siswa ?>">
+                                                <?php echo $row->nama_siswa ?>
+                                            </option>
+                                            <?php endforeach ?>
+                                        </select>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="pembayaran">Jenis Pembayaran</label>
+                                        <select class="form-control" id="pembayaran" name="pembayaran">
+                                            <option selected value="<?php echo $data_pembayaran->jenis_pembayaran ?>">
+                                                <?php echo $data_pembayaran->jenis_pembayaran ?>
+                                            </option>
+                                            <option value="Pembayaran SPP">Pembayaran SPP</option>
+                                            <option value="Pembayaran Uang Gedung">Pembayaran Uang Gedung</option>
+                                            <option value="Pembayaran Uang Seragam">Pembayaran Uang Seragam</option>
+                                        </select>
+
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="total">Total Pembayaran</label>
+                                        <input type="text" class="form-control" id="total" name="total"
+                                            placeholder="Masukkan Total"
+                                            value="<?php echo $data_pembayaran->total_pembayaran ?>">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </form>
+                        <?php endforeach ?>
+                    </div>
+                </div>
             </div>
-            <div class="mb-3 col-6 ">
-            <label for="total_pembayaran" class="form-label"><b>Total Pembayaran</b></label>
-            <input type="text" class="form-control" id="total_pembayaran" placeholder="Total Pembayaran" name="total_pembayaran">
-            <br>
         </div>
-<button type="submit" class="btn btn-sm btn-secondary">Ubah</button>             
-        </form>
-    </div>
+
 </body>
 
 </html>
